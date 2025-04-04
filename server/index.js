e111358bff806fab3291e96d7743ffe4356bd88f
Sangyeon <sangyeon217@gmail.com>
2025-04-04 11:45:22 +0900

    리액트 앱 express에서 서빙

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const path = require('path');
+const fs = require('fs');
+
 const app = express()
 
 app.set('port', 5001);
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
+
+// 리액트 앱 정적파일 서빙
+const buildPath = path.join(__dirname, '../client/build');
+if (fs.existsSync(buildPath) && fs.readdirSync(buildPath).length > 0) {
+  app.use(express.static(buildPath));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+  });
+} else {
+  res.send('Build in progress...')
+}
 
 app.listen(app.get('port'), () => {
     console.log(`Server listening on port ${app.get('port')}`);
